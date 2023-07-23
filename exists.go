86e6037866b800075d7a194dd3614d310312d204
whatwewant <tobewhatwewant@outlook.com>
2023-07-24 07:32:13 +0800

    fix(database): Exists if record not found is normal logic

--- exists.go
+++ exists.go
@@ -1,9 +1,19 @@
 package gormx
 
+import (
+	"errors"
+
+	"gorm.io/gorm"
+)
+
 // Exists returns true if the record exists.
 func Exists[T any](where map[any]any) (bool, error) {
 	_, err := FindOne[T](where)
 	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return false, nil
+		}
+
 		return false, err
 	}
 
