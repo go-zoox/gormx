dced77a37a2799032f3c5469dd76374fed115203
Zero <tobewhatwewant@gmail.com>
2023-09-05 15:45:46 +0800

    feat: support full text search

--- where.go
+++ where.go
@@ -12,6 +12,9 @@ type WhereOne struct {
 	IsFuzzy bool
 	isNot   bool
 	isIn    bool
+	//
+	IsFullTextSearch     bool
+	FullTextSearchFields []string
 }
 
 // Where is the where.
@@ -19,20 +22,32 @@ type Where []WhereOne
 
 // SetWhereOptions is the options for SetWhere.
 type SetWhereOptions struct {
-	IsFuzzy bool
-	IsNot   bool
-	IsIn    bool
+	IsFuzzy              bool
+	IsNot                bool
+	IsIn                 bool
+	IsFullTextSearch     bool
+	FullTextSearchFields []string
 }
 
 // Set sets a where.
-func (w *Where) Set(key string, value interface{}, opts ...*SetWhereOptions) {
+func (w *Where) Set(key string, value interface{}, opts ...*SetWhereOptions) error {
 	var isFuzzy bool
 	var isNot bool
 	var isIn bool
+	var isFullTextSearch bool
+	var fullTextSearchFields []string
 	if len(opts) > 0 && opts[0] != nil {
 		isFuzzy = opts[0].IsFuzzy
 		isNot = opts[0].IsNot
 		isIn = opts[0].IsIn
+		isFullTextSearch = opts[0].IsFullTextSearch
+		fullTextSearchFields = opts[0].FullTextSearchFields
+	}
+
+	if isFullTextSearch {
+		if len(fullTextSearchFields) == 0 {
+			return fmt.Errorf("FullTextSearchFields is required when IsFullTextSearch is true (key: %s)", key)
+		}
 	}
 
 	*w = append(*w, WhereOne{
@@ -41,7 +56,12 @@ func (w *Where) Set(key string, value interface{}, opts ...*SetWhereOptions) {
 		IsFuzzy: isFuzzy,
 		isNot:   isNot,
 		isIn:    isIn,
+		//
+		IsFullTextSearch:     isFullTextSearch,
+		FullTextSearchFields: fullTextSearchFields,
 	})
+
+	return nil
 }
 
 // Get gets a where.
@@ -75,18 +95,36 @@ func (w *Where) Build() (string, []interface{}) {
 	whereClauses := []string{}
 	whereValues := []interface{}{}
 	for _, w := range *w {
-		if w.IsFuzzy {
-			whereClauses = append(whereClauses, fmt.Sprintf("%s ILike ?", w.Key))
-			whereValues = append(whereValues, fmt.Sprintf("%%%s%%", w.Value))
-		} else if w.isNot {
-			whereClauses = append(whereClauses, fmt.Sprintf("%s != ?", w.Key))
-			whereValues = append(whereValues, w.Value)
-		} else if w.isIn {
-			whereClauses = append(whereClauses, fmt.Sprintf("%s in (?)", w.Key))
-			whereValues = append(whereValues, w.Value)
+		// @TODO full text search search keyword
+		if w.IsFullTextSearch {
+			keywordExtract := strings.Replace(w.Key, ":*", "", 1)
+			keywordFuzzy := fmt.Sprintf("%%%s%%", keywordExtract)
+			qs := []string{}
+			args := []interface{}{}
+
+			fields := w.FullTextSearchFields
+			for _, field := range fields {
+				qs = append(qs, fmt.Sprintf("%s ILike ?", field))
+				args = append(args, keywordFuzzy)
+			}
+			query := strings.Join(qs, " OR ")
+
+			whereClauses = append(whereClauses, fmt.Sprintf("(%s)", query))
+			whereValues = append(whereValues, args...)
 		} else {
-			whereClauses = append(whereClauses, fmt.Sprintf("%s = ?", w.Key))
-			whereValues = append(whereValues, w.Value)
+			if w.IsFuzzy {
+				whereClauses = append(whereClauses, fmt.Sprintf("%s ILike ?", w.Key))
+				whereValues = append(whereValues, fmt.Sprintf("%%%s%%", w.Value))
+			} else if w.isNot {
+				whereClauses = append(whereClauses, fmt.Sprintf("%s != ?", w.Key))
+				whereValues = append(whereValues, w.Value)
+			} else if w.isIn {
+				whereClauses = append(whereClauses, fmt.Sprintf("%s in (?)", w.Key))
+				whereValues = append(whereValues, w.Value)
+			} else {
+				whereClauses = append(whereClauses, fmt.Sprintf("%s = ?", w.Key))
+				whereValues = append(whereValues, w.Value)
+			}
 		}
 	}
 	whereClause := strings.Join(whereClauses, " AND ")
