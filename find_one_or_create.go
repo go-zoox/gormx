dbf165e63abe72ae3e1a96430d650f401215aad7
Zero <tobewhatwewant@gmail.com>
2023-12-11 17:09:51 +0800

    fix: find_one_or_create only if record not found

--- find_one_or_create.go
+++ find_one_or_create.go
@@ -1,9 +1,19 @@
 package gormx
 
+import (
+	"errors"
+
+	"gorm.io/gorm"
+)
+
 // FindOneOrCreate find one or create one.
 func FindOneOrCreate[T any](where map[any]any, callback func(*T)) (*T, error) {
 	f, err := FindOne[T](where)
 	if err != nil {
+		if !errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, err
+		}
+
 		var tmp T
 		callback(&tmp)
 
